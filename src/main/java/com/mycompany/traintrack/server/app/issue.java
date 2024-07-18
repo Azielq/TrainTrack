7fc24139106bb282992de07c4d4d738a25043aa0
Gusttav28 <gusttav28@gmail.com>
2024-07-18 13:46:35 -0600

    subiendo esto

---
+++ src/main/java/com/mycompany/traintrack/server/app/issue.java
@@ -0,0 +1,7 @@
+package com.mycompany.traintrack.server.app;
+
+public class issue {
+    public issue(){
+        System.out.println("Se arreglo el issue");
+    }
+}
---
+++ src/main/java/com/mycompany/traintrack/server/app/testForBranchDev.java
@@ -0,0 +1,8 @@
+package com.mycompany.traintrack.server.app;
+
+public class testForBranchDev {
+    public testForBranchDev(){
+        System.out.println("testeo");
+        // SDf
+    }
+}
