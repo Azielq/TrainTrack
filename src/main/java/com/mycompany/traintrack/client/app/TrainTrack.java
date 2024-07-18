5d6f3258473baf1a15ae850f174e2e1dc11a483d
Azielq <azielqsd@gmail.com>
2024-07-17 21:32:01 -0600

    First commit, launching the project

---
+++ src/main/java/com/mycompany/traintrack/client/app/TrainTrack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.traintrack.client.app;
+
+/**
+ *
+ * @author aziel
+ */
+public class TrainTrack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
