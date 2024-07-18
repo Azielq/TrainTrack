0ac8e36bc3513582d027b21acbc5bbd341366eff
Azielq <azielqsd@gmail.com>
2024-07-17 21:35:50 -0600

    Inicio del main branch

--- src/main/java/com/mycompany/traintrack/client/app/TrainTrack.java
+++ src/main/java/com/mycompany/traintrack/client/app/TrainTrack.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.traintrack.client.app;
 
-/**
- *
- * @author aziel
- */
 public class TrainTrack {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        System.out.println("Hola, este es el repo del proyecto de concurrente");
+
     }
 }
